60cf8eba8334f5423045a8687df504852ff96abc
Andrey9300 <andrey93077@gmail.com>
2025-11-01 21:35:48 +0100

    feat: fix build

--- src/app/demo/advanced-integration/helpers/apstagHelper.ts
+++ src/app/demo/advanced-integration/helpers/apstagHelper.ts
@@ -2,16 +2,16 @@ import { AUCTION_TIMEOUT } from '../constants/timeout'
 import { AMAZON_UNITS } from '../constants/units'
 
 export const defineApstag = () => {
-  const addToQueue = (c, r) => {
+  const addToQueue = (c: any, r: any) => {
     // eslint-disable-next-line no-underscore-dangle
     window.apstag._Q.push([c, r])
   }
 
   window.apstag = {
-    init(...rest) {
+    init(...rest: any) {
       addToQueue('i', rest)
     },
-    fetchBids(...rest) {
+    fetchBids(...rest: any) {
       addToQueue('f', rest)
     },
     // eslint-disable-next-line @typescript-eslint/no-empty-function
