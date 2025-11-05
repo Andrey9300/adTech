28527522989390616462bb6df4a3a4a3637114ad
Andrey9300 <andrey93077@gmail.com>
2025-11-05 15:16:06 +0100

    feat: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,3 @@
-import js from '@eslint/js'
-import nextPlugin from '@next/eslint-plugin-next'
-import globals from 'globals'
+import {defineConfig} from "eslint/config";
 
-export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    ignores: ['node_modules', '.next', 'dist'],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      '@next/next': nextPlugin,
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      '@next/next/no-html-link-for-pages': 'off',
-    },
-  },
-]
+export default defineConfig([]);
