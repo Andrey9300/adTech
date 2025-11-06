dd89fb5f888d4391103e4806b01d8c403cb8ead6
Andrey9300 <andrey93077@gmail.com>
2025-11-06 14:05:47 +0100

    feat: add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,39 @@
-import type { Metadata } from "next";
-import "../styles/globals.css";
+import type { Metadata } from 'next'
+import '../styles/globals.css'
 
 export const metadata: Metadata = {
-  title: "Ad tech",
-  description: "Ad tech",
-};
+  title: 'Ad tech',
+  description: 'Ad tech',
+}
 
 interface LayoutProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/16x16.png"
+      />
+      <link rel="manifest" href="manifest.json" />
       <body className="min-h-screen flex flex-col bg-gray-50 text-gray-900">
         {children}
       </body>
     </html>
-  );
+  )
 }
