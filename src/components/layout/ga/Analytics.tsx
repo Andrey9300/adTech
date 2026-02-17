78646a7ffe2b5aa6a880cbeeaf8d20cd4d258ffa
Andrey9300 <andrey93077@gmail.com>
2026-02-17 21:29:14 +0100

    feat: add gdpr to all pages

--- src/components/layout/ga/Analytics.tsx
+++ src/components/layout/ga/Analytics.tsx
@@ -1,17 +1,26 @@
 'use client'
 import { useEffect } from 'react'
 import { loadAsync } from '@/lib/loadAsync'
+import { initCmp, onTcfReady } from '@/features/tcf'
 
 export const Analytics = () => {
   useEffect(() => {
-    loadAsync('https://www.googletagmanager.com/gtag/js?id=G-MXV3H81NJS')
+    initCmp()
 
-    window.dataLayer = window.dataLayer || []
-    function gtag(paramStr: any, param: any) {
-      window.dataLayer.push(arguments)
-    }
-    gtag('js', new Date())
-    gtag('config', 'G-MXV3H81NJS')
+    onTcfReady(({ hasPurposes }) => {
+      if (!hasPurposes) {
+        return
+      }
+
+      loadAsync('https://www.googletagmanager.com/gtag/js?id=G-MXV3H81NJS')
+
+      window.dataLayer = window.dataLayer || []
+      function gtag(paramStr: any, param: any) {
+        window.dataLayer.push(arguments)
+      }
+      gtag('js', new Date())
+      gtag('config', 'G-MXV3H81NJS')
+    })
   }, [])
 
   return null
